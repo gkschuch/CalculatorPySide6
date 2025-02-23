0a941aacda81fbcc8930ba976a5e14a2a411f0f3
Gabriel Karini Schuch <gksgabriel2004@gmail.com>
2025-02-23 20:31:28 -0300

    Configurando o botao igual e o resto da conta (lado direito)

--- src/buttons.py
+++ src/buttons.py
@@ -74,10 +74,14 @@ class ButtonsGrid(QGridLayout):
 
         if text == 'C':
             self._connectButtonClicked(button, self._clear)
+
         if text in '+-/*':
             self._connectButtonClicked(
                 button, self._makeSlot(self._operatorClicked, button)
             )
+
+        if text == '=':
+            self._connectButtonClicked(button, self._eq)
 
     def _makeSlot(self, method, *args, **kwargs):
         @Slot(bool)
@@ -114,3 +118,23 @@ class ButtonsGrid(QGridLayout):
 
         self._op = buttonText
         self.equation = f'{self._left} {self._op} ??'
+
+    def _eq(self,):
+        displayText = self.display.text()
+
+        if not isValidNumber(displayText):
+            return
+
+        self._right = float(displayText)
+        self.equation = f'{self._left} {self._op} {self._right}'
+        result = 0.0
+
+        try:
+            result = eval(self.equation)
+        except ZeroDivisionError:
+            print('Zero Division Error')
+
+        self.display.clear()
+        self.info.setText(f'{self.equation} = {result}')
+        self._left = result
+        self._right = None
