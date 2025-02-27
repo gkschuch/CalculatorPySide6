0639c83ddc74d93045d5e96bf1a72cc12ca7a6dc
Gabriel Karini Schuch <gksgabriel2004@gmail.com>
2025-02-27 09:22:57 -0300

    Organizando os arquivos de configuração da calculadora

---
+++ src/configCalculator.py
@@ -0,0 +1,308 @@
+from PySide6.QtCore import Slot, Qt, Signal
+from PySide6.QtWidgets import (QGridLayout, QPushButton, QMainWindow,
+                               QMessageBox, QVBoxLayout, QWidget, QLineEdit,
+                               QLabel)
+from utils import isEmpty, isNumOrDot, isValidNumber, converToNumber
+from variables import (MEDIUM_FONT_SIZE, SMALL_FONT_SIZE, BIG_FONT_SIZE,
+                       TEXT_MARGIN, MINIMUN_WIDTH)
+from PySide6.QtGui import QKeyEvent
+import math
+
+
+class Button(QPushButton):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.configStyle()
+
+    def configStyle(self):
+        font = self.font()
+        font.setPixelSize(MEDIUM_FONT_SIZE)
+        self.setFont(font)
+        self.setMinimumSize(75, 75)
+
+
+class ButtonsGrid(QGridLayout):
+    def __init__(
+            self, display: 'Display', info: 'Info', window: 'MainWindow',
+            *args, **kwargs
+    ) -> None:
+        super().__init__(*args, **kwargs)
+
+        self._gridMask = [
+            ['C', '⌫', '^', '/'],
+            ['7', '8', '9', '*'],
+            ['4', '5', '6', '-'],
+            ['1', '2', '3', '+'],
+            ['N',  '0', '.', '='],
+        ]
+        self.display = display
+        self.info = info
+        self.window = window
+        self._equation = ''
+        self._equationInitialValue = 'Sua conta'
+        self._left = None
+        self._right = None
+        self._op = None
+
+        self.equation = self._equationInitialValue
+        self._makeGrid()
+
+    @property
+    def equation(self):
+        return self._equation
+
+    @equation.setter
+    def equation(self, value):
+        self._equation = value
+        self.info.setText(value)
+
+    def _makeGrid(self):
+        self.display.eqPressed.connect(self._eq)
+        self.display.delPressed.connect(self._backspace)
+        self.display.clearPressed.connect(self._clear)
+        self.display.inputPressed.connect(self._insertToDisplay)
+        self.display.operatorPressed.connect(self._configLeftOp)
+        self.display.negativePressed.connect(self._invertNumber)
+
+        for rowNumber, rowData in enumerate(self._gridMask):
+            for colNumber, buttonText in enumerate(rowData):
+                button = Button(buttonText)
+
+                if not isNumOrDot(buttonText) and not isEmpty(buttonText):
+                    button.setProperty('cssClass', 'specialButton')
+                    self._configSpecialButton(button)
+
+                self.addWidget(button, rowNumber, colNumber)
+                slot = self._makeSlot(self._insertToDisplay, buttonText)
+                self._connectButtonClicked(button, slot)
+
+    def _connectButtonClicked(self, button, slot):
+        button.clicked.connect(slot)  # type: ignore
+
+    def _configSpecialButton(self, button):
+        text = button.text()
+
+        if text == 'C':
+            self._connectButtonClicked(button, self._clear)
+
+        if text == '⌫':
+            self._connectButtonClicked(button, self._backspace)
+
+        if text == 'N':
+            self._connectButtonClicked(button, self._invertNumber)
+
+        if text in '+-/*^':
+            self._connectButtonClicked(
+                button,
+                self._makeSlot(self._configLeftOp, text)
+            )
+
+        if text == '=':
+            self._connectButtonClicked(button, self._eq)
+
+    @Slot()  # type: ignore
+    def _makeSlot(self, func, *args, **kwargs):
+        @Slot(bool)
+        def realSlot(_):
+            func(*args, **kwargs)
+        return realSlot
+
+    @Slot()
+    def _invertNumber(self):
+        displayText = self.display.text()
+
+        if not isValidNumber(displayText):
+            return
+
+        number = converToNumber(displayText) * -1
+        self.display.setText(str(number))
+        self.display.setFocus()
+
+    @Slot()  # type: ignore
+    def _insertToDisplay(self, text):
+        newDisplayValue = self.display.text() + text
+
+        if not isValidNumber(newDisplayValue):
+            return
+
+        self.display.insert(text)
+        self.display.setFocus()
+
+    @Slot()
+    def _clear(self):
+        self._left = None
+        self._right = None
+        self._op = None
+        self.equation = self._equationInitialValue
+        self.display.clear()
+        self.display.setFocus()
+
+    @Slot()  # type: ignore
+    def _configLeftOp(self, text: str) -> None:
+        displayText = self.display.text()  # Deverá ser meu número _left
+        self.display.clear()  # Limpa o display
+
+        # Se a pessoa clicou no operador sem
+        # configurar qualquer número
+        if not isValidNumber(displayText) and self._left is None:
+            self._showError('Você não digitou nada.')
+            return
+
+        # Se houver algo no número da esquerda,
+        # não fazemos nada. Aguardaremos o número da direita.
+        if self._left is None:
+            self._left = converToNumber(displayText)
+
+        self._op = text
+        self.equation = f'{self._left} {self._op} ??'
+        self.display.setFocus()
+
+    @Slot()
+    def _eq(self):
+        displayText = self.display.text()
+
+        if not isValidNumber(displayText) or self._left is None:
+            self._showError('Conta incompleta.')
+            return
+
+        self._right = converToNumber(displayText)
+        self.equation = f'{self._left} {self._op} {self._right}'
+        result = 'error'
+
+        try:
+            if '^' in self.equation and isinstance(self._left, int | float):
+                result = math.pow(self._left, self._right)
+                result = converToNumber(str(result))
+            else:
+                result = eval(self.equation)
+        except ZeroDivisionError:
+            self._showError('Divisão por zero.')
+        except OverflowError:
+            self._showError('Essa conta não pode ser realizada.')
+
+        self.display.clear()
+        self.info.setText(f'{self.equation} = {result}')
+        self._left = result
+        self._right = None
+        self.display.setFocus()
+
+        if result == 'error':
+            self._left = None
+
+    @Slot()
+    def _backspace(self):
+        self.display.backspace()
+        self.display.setFocus()
+
+    def _makeDialog(self, text):
+        msgBox = self.window.makeMsgBox()
+        msgBox.setText(text)
+        return msgBox
+
+    def _showError(self, text):
+        msgBox = self._makeDialog(text)
+        msgBox.setIcon(msgBox.Icon.Critical)
+        msgBox.exec()
+
+    def _showInfo(self, text):
+        msgBox = self._makeDialog(text)
+        msgBox.setIcon(msgBox.Icon.Information)
+        msgBox.exec()
+
+
+class Display(QLineEdit):
+    eqPressed = Signal()
+    delPressed = Signal()
+    clearPressed = Signal()
+    inputPressed = Signal(str)
+    operatorPressed = Signal(str)
+    negativePressed = Signal()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.configStyle()
+
+    def configStyle(self):
+        margins = [TEXT_MARGIN for _ in range(4)]
+        self.setStyleSheet(f'font-size: {BIG_FONT_SIZE}px;')
+        self.setMinimumHeight(BIG_FONT_SIZE * 2)
+        self.setMinimumWidth(MINIMUN_WIDTH)
+        self.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.setTextMargins(*margins)
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        text = event.text().strip()
+        key = event.key()
+        KEYS = Qt.Key
+
+        isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return, KEYS.Key_Equal]
+        isDelete = key in [KEYS.Key_Backspace, KEYS.Key_Delete, KEYS.Key_D]
+        isEsc = key in [KEYS.Key_Escape, KEYS.Key_C]
+        isOperator = key in [
+            KEYS.Key_Plus, KEYS.Key_Minus, KEYS.Key_Slash, KEYS.Key_Asterisk,
+            KEYS.Key_P,
+        ]
+        isNegative = key in [KEYS.Key_N]
+
+        if isEnter and KEYS.Key_Plus != '+':
+            self.eqPressed.emit()
+            return event.ignore()
+
+        if isDelete:
+            self.delPressed.emit()
+            return event.ignore()
+
+        if isEsc:
+            self.clearPressed.emit()
+            return event.ignore()
+        if isNegative:
+            self.negativePressed.emit()
+            return event.ignore()
+
+        if isOperator:
+            if text.lower() == 'p':
+                text = '^'
+            self.operatorPressed.emit(text)
+            return event.ignore()
+
+        if isEmpty(text):
+            return event.ignore()
+
+        if isNumOrDot(text):
+            self.inputPressed.emit(text)
+            return event.ignore()
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
+        super().__init__(parent, *args, **kwargs)
+
+        # Configurando o layout básico
+        self.cw = QWidget()
+        self.vLayout = QVBoxLayout()
+        self.cw.setLayout(self.vLayout)
+        self.setCentralWidget(self.cw)
+
+        # Título da janela
+        self.setWindowTitle('Calculadora')
+
+    def adjustFixedSize(self):
+        # Última coisa a ser feita
+        self.adjustSize()
+        self.setFixedSize(self.width(), self.height())
+
+    def addWidgetToVLayout(self, widget: QWidget):
+        self.vLayout.addWidget(widget)
+
+    def makeMsgBox(self):
+        return QMessageBox(self)
+
+
+class Info(QLabel):
+    def __init__(self, text: str, parent: QWidget | None = None) -> None:
+        super().__init__(text, parent)
+        self.configStyle()
+
+    def configStyle(self):
+        self.setStyleSheet(f'font-size: {SMALL_FONT_SIZE}px;')
+        self.setAlignment(Qt.AlignmentFlag.AlignRight)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
 import sys
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
-from main_window import MainWindow
-from display import Display
-from info import Info
+from configCalculator import MainWindow, Display, Info, ButtonsGrid
 from styles import setupTheme
-from buttons import ButtonsGrid
 from variables import WINDOW_ICON_PATH
 
 if __name__ == '__main__':
